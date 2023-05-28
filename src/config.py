85506b306d1bdfbeb41f7cf2b1ea765935e77c06
Tom Nordal <tnordal@gmail.com>
2023-05-28 07:23:07 +0200

    clean config

--- src/config.py
+++ src/config.py
@@ -2,11 +2,6 @@
 
 import os
 
-# from pathlib import Path  # pathlib is seriously awesome!
-# data_dir = Path('/path/to/some/logical/parent/dir')
-# data_path = data_dir / 'my_file.csv'  # use feather files if possible!!!
-
-
 BASE_DIR = r"C:\Users\tnord\OneDrive\Developer\py\trade_worst_paper"
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
