161290a38899933bd358e390136c9677f42ffeda
Tom Nordal <tnordal@gmail.com>
2023-05-28 09:38:59 +0200

    add netfonds_files

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ BASE_DIR = r"C:\Users\tnord\OneDrive\Developer\py\trade_worst_paper"
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 STOCK_DATA = os.path.join(DATA_DIR, 'stock_data')
 TICKER_FILES = os.path.join(DATA_DIR, 'ticker_files')
+NETFONDS_FILES = os.path.join(DATA_DIR, 'netfonds')
 
 REPORTS_DIR = os.path.join(BASE_DIR, 'reports')
 ERROR_LOGS = os.path.join(REPORTS_DIR, 'error_logs')
